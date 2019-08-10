234c8dd915ac1e74ad0b2b5b8447943857aebb03
Francis Fulgencio <francisfulgencio@gmail.com>
2019-08-09 20:49:03 -0400

    primera carga de archivos

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import render_template
+from flask import request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', 
+                            name = 'Mark Zuckember',
+                            email = 'mz@facebook.com',
+                            phone = '18009201516')
+
+@app.route('/products/<int:id>')
+def getProductById(id, methods=['GET','POST']):
+    print(nombre)
+    products = ['Mause', 'Teclado','Procesador','tarjeta de video']
+    return render_template('product-detail.html', product = products[id], products = products)
+
+
+@app.route('/form')
+def formulario():
+    return render_template('form.html')
+
+@app.route('/form/procesar',methods=['POST','GET'])
+def procesarForm():
+    nombre = request.form['nombre']
+    telefono = request.form['telefono']
+    return jsonify(request.form)
+
+
+@app.route('/empleados')
+def getEmpleados():
+    empleados = [
+        {'nombre':'juan', 'telefono':'809-476-6000'},
+        {'nombre':'Manuel', 'telefono':'809-476-6000'},
+        {'nombre':'Jose', 'telefono':'809-476-6000'},
+        {'nombre':'Ramon', 'telefono':'809-476-6000'},
+    ]
+
+    return render_template('empleados.html', empleados=empleados)
+
+if  __name__ == '__main__':
+    app.run(port=4200)
